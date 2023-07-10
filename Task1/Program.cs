044e208bbbd8067ed625cfc092de8aa3e81ef831
vmrozhkovsky <128725885+vmrozhkovsky@users.noreply.github.com>
2023-07-10 16:51:49 +0300

    Проект Classrooms переименован в Task1 Добавлен код проекта Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new []
+            {
+                new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+                new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+                new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
+            
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+        static string [] GetAllStudents( Classroom [] classes )
+        {
+            var result = classes.SelectMany(cls => cls.Students.Select(name=> name)).ToArray();
+            return result;
+        }
+      
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
